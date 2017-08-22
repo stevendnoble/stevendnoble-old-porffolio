a99e3a5ca2c26162e0e8643ce59e542ef534c441
stevenrocksbox <steven@rocksbox.com>
2017-08-22 03:13:05 -0700

    responsiveness, css transitions

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,47 @@
 $(function() {
   var $pageLinks = $('a.page-link'),
-      $links = $('.links');
+      $links = $('.links'),
+      $windowWidth = $(window).width(),
+      $windowHeight = $(window).height(),
+      imageId = 0,
+      firstPageLoad = true,
+      socialButtonWidth = Math.floor(($windowWidth - 114) / 7),
+      buttonPanelWidth = (socialButtonWidth + 12) * 7,
+      navHeight = socialButtonWidth + 16,
+      navPadding = ($windowWidth - buttonPanelWidth - 16) / 2,
+      maxHeight = $windowHeight - navHeight;
 
-  var imageId = 0;
+  function animateSidePanel() {
+    $('.sub-panel').removeClass('p24').addClass('p8');
+    $('.social-buttons-3, .social-buttons-4').css('display', 'inline');
+    $('.button-link').css('width', String(socialButtonWidth) + 'px')
+                     .css('height', String(socialButtonWidth) + 'px');
+    $('.social-buttons').css('marginLeft', String(navPadding) + 'px');
+    if (socialButtonWidth < 32) {
+      $('.fa-2x').removeClass('fa-2x');
+    }
+    $('.name-and-picture').animate({ height: 0 }, 1000, animateNav);
+  }
+
+  function animateNav() {
+    $('.name-and-picture').hide();
+    $('.sub-panel').animate( {height: String(navHeight) + 'px' }, 1000, modifyFullHeight);
+  }
+
+  function modifyFullHeight() {
+    $('.full-height').css('max-height', String(maxHeight) + 'px');
+    $('.sub-panel').removeClass('full-height');
+  }
 
   function switchTab() {
     event.preventDefault();
-    $links.hide();
     var show = $(this).attr('href');
+    $links.hide();
     $(show).show();
+    if (firstPageLoad && $windowWidth <= 576) {
+      firstPageLoad = false;
+      animateSidePanel();
+    }
   }
 
   function switchImage() {
@@ -20,5 +53,4 @@ $(function() {
   $pageLinks.on('click', switchTab);
   
   var intervalID = setInterval(switchImage, 3000);
-
 });
